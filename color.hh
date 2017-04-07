22c5e3a41984582bde94e7c2707913a90f7ac78e
Jérôme Kunegis <kunegis@gmail.com>
2017-04-07 10:48:27 +0200

    detach flags.hh; document coding

--- color.hh
+++ color.hh
@@ -40,6 +40,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 class Color 
 {
--- dependency.hh
+++ dependency.hh
@@ -9,134 +9,11 @@
  * Dependency, and are used via shared_ptr<>.    
  */
 
-#include <limits.h>
-
 #include <map>
-#include <memory>
-#include <string>
 
 #include "error.hh"
 #include "target.hh"
-#include "format.hh"
-
-/*
- * Flags are represented in Stu files with a syntax that resembles
- * command line options, i.e., -p, -o, etc.  Internally, flags are
- * defined as bit fields. 
- *
- * Each edge in the dependency graph is annotated with one
- * object of this type.  This contains bits related to what should be
- * done with the dependency, whether time is considered, etc.  The flags
- * are defined in such a way that the simplest dependency is
- * represented by zero, and each flag enables an optional feature.  
- *
- * The transitive bits effectively are set for tasks not to do.
- * Therefore, inverting them gives the bits for the tasks to do.   
- *
- * Declared as integer so arithmetic can be performed on it.
- */
-typedef unsigned Flags; 
-enum 
-{
-	/* The index of the flags, used for array indexing.  Variables
-	 * iterating over these values are usually called I.  */ 
-	I_PERSISTENT       = 0,
-	I_OPTIONAL,         
-	I_TRIVIAL,          
-	I_DYNAMIC,
-	I_CONCATENATE,
-	I_VARIABLE,
-	I_OVERRIDE_TRIVIAL,
-	I_NEWLINE_SEPARATED,
-	I_NUL_SEPARATED,
-
-	C_ALL, /* Total number of flags */ 
-
-	/* Base bit and length of the concatenation index numbers */
-	C_CONCATENATE_BASE  = C_ALL,
-	C_CONCATENATE_COUNT = CHAR_BIT * sizeof(unsigned) - C_ALL,
-	C_CONCATENATE_MAX = (1 << C_CONCATENATE_COUNT) - 1,
-
-	C_TRANSITIVE       = 3,
-	/* The first C_TRANSITIVE flags are transitive, i.e., inherited
-	 * across transient targets  */
-
-	/* What follows are the actual flag bits to be ORed together */ 
-
-	/* 
-	 * Transitive flags
-	 */ 
-
-	F_PERSISTENT       = 1 << I_PERSISTENT,  
-	/* (-p) When the dependency is newer than the target, don't rebuild */ 
-
-	F_OPTIONAL         = 1 << I_OPTIONAL,
-	/* (-o) Don't create the dependency if it doesn't exist */
-
-	F_TRIVIAL          = 1 << I_TRIVIAL,
-	/* (-t) Trivial dependency */
-
-	/* 
-	 * Intransitive flags
-	 */ 
-
-	F_DYNAMIC          = 1 << I_DYNAMIC,  
-	/* This is a [...[X]...]->X special dynamic link */
-
-	F_CONCATENATE      = 1 << I_CONCATENATE,
-	/* This is a link between a Concatenating_Execution and its
-	 * child Execution in Stage 0.  */
-
-	F_VARIABLE         = 1 << I_VARIABLE,
-	/* ($[...]) Content of file is used as variable */ 
-
-	F_OVERRIDE_TRIVIAL = 1 << I_OVERRIDE_TRIVIAL,
-	/* Used only in Link.flags in the second pass.  Not used for
-	 * dependencies.  Means to override all trivial flags. */ 
-
-	F_NEWLINE_SEPARATED= 1 << I_NEWLINE_SEPARATED,
-	/* For dynamic dependencies, the file contains newline-separated
-	 * filenames, without any markup  */ 
-
-	F_NUL_SEPARATED=    1 << I_NUL_SEPARATED,
-	/* For dynamic dependencies, the file contains NUL-separated
-	 * filenames, without any markup  */ 
-};
-
-const char *const FLAGS_CHARS= "pot`$*n0"; 
-/* Characters representing the individual flags -- used in verbose mode
- * output */ 
-
-int flag_get_index(char c)
-/* 
- * Get the flag index corresponding to a character.
- */ 
-{
-	switch (c) {
-	case 'p':  return I_PERSISTENT;
-	case 'o':  return I_OPTIONAL;
-	case 't':  return I_TRIVIAL;
-	case 'n':  return I_NEWLINE_SEPARATED;
-	case '0':  return I_NUL_SEPARATED;
-		
-	default:
-		assert(false);
-		return 0;
-	}
-}
-
-string flags_format(Flags flags) 
-/* 
- * Textual representation of a flags value.  To be shown before the
- * argument.  Empty when flags are empty. 
- */
-{
-	string ret= "";
-	for (int i= 0;  i < C_ALL;  ++i)
-		if (flags & (1 << i))
-			ret += frmt("-%c ", FLAGS_CHARS[i]); 
-	return ret;
-}
+#include "flags.hh"
 
 class Dependency
 /* 
@@ -157,6 +34,11 @@ class Dependency
  *
  * The flags only represent immediate flags.  Compound dependencies for
  * instance may contain additional inner flags. 
+ *
+ * Objects of type Dependency and subclasses are always handled through
+ * shared_ptr<>.  All objects may have many persistent pointers to it,
+ * so they are considered final, i.e., immutable, except if we just
+ * created the object in which case we know that it is not shared.  
  */ 
 {
 public:

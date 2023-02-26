a232cef1cf66d24fd4b32d0db6b807e9c0451c12
J Kunegis <kunegis@gmail.com>
2023-02-26 16:13:45 +0100

    update year, don't mention Namur

--- stu.cc
+++ stu.cc
@@ -86,7 +86,7 @@ const char HELP[]=
 
 const char VERSION_INFO[]=
 	PACKAGE " " STU_VERSION "\n"
-	"Copyright (C) 2014-2022 Jerome Kunegis\n"
+	"Copyright (C) 2014-2023 Jerome Kunegis\n"
 	"License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
 	"This is free software: you are free to change and redistribute it.\n"
 	"There is NO WARRANTY, to the extent permitted by law.\n";

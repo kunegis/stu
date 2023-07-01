638d44fe389b1cf8cf099daef83a379c888e125a
Jérôme KUNEGIS <kunegis@gmail.com>
2023-07-01 20:22:54 +0200

    sh simplification

--- src/version.hh
+++ src/version.hh
@@ -3,9 +3,9 @@
 #ifndef VERSION_HH
 #define VERSION_HH
 
-#define STU_VERSION "2.7.73"
+#define STU_VERSION "2.7.74"
 #define STU_VERSION_MAJOR 2
 #define STU_VERSION_MINOR 7
-#define STU_VERSION_PATCH 73
+#define STU_VERSION_PATCH 74
 
 #endif

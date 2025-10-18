e3689e870336029211d362e4dfa5234d186715f3
Jérôme KUNEGIS <kunegis@gmail.com>
2025-10-18 09:56:56 +0200

    2.7.226-0006

---
+++ tests/2.7.226-0006/PRELOAD.cc
@@ -0,0 +1,27 @@
+#include <dlfcn.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+static FILE *file_schtroumpf= NULL;
+
+extern "C"
+FILE *fopen(const char *pathname, const char *mode)
+{
+	FILE *ret= ((FILE * (*)(const char *, const char *))dlsym(RTLD_NEXT, "fopen"))
+		(pathname, mode);
+	if (ret && !strcmp(pathname, "SCHTROUMPF")) {
+		file_schtroumpf= ret;
+	}
+	return ret;
+}
+
+extern "C"
+int fclose(FILE *stream)
+{
+	if (file_schtroumpf && stream == file_schtroumpf) {
+		errno= ENOMEM;
+		return EOF;
+	}
+	return ((int (*)(FILE *))dlsym(RTLD_NEXT, "fclose"))(stream);
+}

b41b291deb8410fedbbb6d2018efe21b0bbfa728
Jérôme KUNEGIS <kunegis@gmail.com>
2025-10-18 09:30:19 +0200

    2.7.226-0002

---
+++ tests/2.7.226-0002/PRELOAD.cc
@@ -0,0 +1,16 @@
+#include <dlfcn.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+extern "C"
+FILE *fopen(const char *pathname, const char *mode)
+{
+	if (!strcmp(pathname, "SCHTROUMPF")) {
+		errno= ENOENT;
+		return NULL;
+	}
+
+	return ((FILE * (*)(const char *, const char *))dlsym(RTLD_NEXT, "fopen"))
+		(pathname, mode);
+}
---
+++ tests/2.7.226-0003/PRELOAD.cc
@@ -0,0 +1,16 @@
+#include <dlfcn.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+extern "C"
+FILE *fopen(const char *pathname, const char *mode)
+{
+	if (!strcmp(pathname, "SCHTROUMPF")) {
+		errno= EACCES;
+		return NULL;
+	}
+
+	return ((FILE * (*)(const char *, const char *))dlsym(RTLD_NEXT, "fopen"))
+		(pathname, mode);
+}
